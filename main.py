55dd28916ad18116548597830703d40c527791c4
Mateus Figueiredo Pereira <figueiredo1497@gmail.com>
2023-11-21 18:58:04 -0300

    Adiciona CORS

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from core.configs import settings
 from api.v1.api import api_router
+
+origins = [
+    "*",
+]
 
 
 app: FastAPI = FastAPI(title='Catalogo de Veiculos API - FastAPI')
 app.include_router(api_router, prefix=settings.API_V1_STR)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == '__main__':
     import uvicorn
